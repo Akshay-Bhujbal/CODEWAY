f47e4d9639321bf5c463532aebe7fedd2ace1323
Akshay-Bhujbal <158591143+Akshay-Bhujbal@users.noreply.github.com>
2024-02-07 16:27:46 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,46 @@
+
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def devide(x, y):
+    if y == 0:
+        return("Error. Division by zero is not allowed")
+    else:
+        return x / y
+
+print("Simple Calculator")
+print("1. Addition")
+print("2. Substraction")
+print("3. Multiplication")
+print("4. Devision")
+
+while True:
+    choice = input("Enter the operation number (1/2/3/4): ")
+
+    if choice in ("1","2","3","4"):
+        n1 = float(input("Enter first number: "))
+        n2 = float(input("Enter second number: "))
+
+        if choice == "1":
+            print("Addition: ", add(n1, n2))
+
+        elif choice == "2":
+            print("Substaction: ", substract(n1, n2))
+
+        elif choice == "3":
+            print("Multiplication: ", multiply(n1, n2))
+
+        elif choice == "4":
+            print("Devision: ", devide(n1, n2))
+    else:
+        print("Invalid Input")
+
+    next_calculation = input("Do you want to perform another calculation? (y/n): ")
+    if next_calculation != "y":
+        break
